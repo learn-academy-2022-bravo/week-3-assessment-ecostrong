dba504073a897d3243453c6d3e8884fc26e268ea
ecostrong <necoarmstrong@mac.com>
2022-04-17 21:43:41 -0700

    made corrections

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -8,173 +8,180 @@
 // Add appropriate dependencies: $ yarn add jest
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
+
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
-//Pseudo Code
-// create a function takes in a number, x 
-// the number must be greater than 2, x > 2
-// in that function create an array that can have numbers in it. 
-// returns an array that lenght 
-// the length is the index, so what ever the number is the index has to match the lenght of the array 
-// the length of Fibonacci sequence 
-// What is Fibonacci sequence? [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
-// var fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
-const myNumber =(x) => {
-let newArray=[]
-    for (var i=0; i <fibonacci.length; i++) {
-        myNumber[i]*=2;
-        newArray.index[myNumber]
-    }
-       return fibonacci }
-        console.log(myNumber(newArray))
-//note: my function is not working in console.log 
-// Jest testing Steps 
-// 1: Write the test first! 
-// 2: Run your testing suit to see a failing test
-// 3: Write the code
-// 4: Run your testing suit to see your test pass
-// 5: Refactor if necessary
-// This is known as Red-Green Refactor
 
 // a) Create a test with expect statements for each of the variables provided.
 //This is a test for a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence
-// describe a method that list the name of the function or the naming of the particular test 
+// describe a method that list the name of the function or the naming of the particular test
 
-describe("myNumber",() => {
-//     //a test/it method, nested within the describe block that in plain words describes the function that does it 
-  // it("takes in a number and returns an array the length containing the numbers of the Fibonacci Sequence", () => {
-//an expect method, nested within the test block, calling the myNumber () function followed by the to.Equal
-    expect(myNumber()).toEqual("array constains Fibonacci Sequance ")
-   })
+describe("fibonacciSequence",() => {
+     it ("takes in a number greater then 2 and returns an array that length containing the numbers of the Fibonacci Sequence", () => {
+         expect (fibonacciSequence(6)).toEqual([1, 1, 2, 3, 5,8])
+         expect (fibonacciSequence(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21,34,55])
+  })
 })
+
 // FAIL  ./code-challenges.test.js
-// myNumber
-//   ✕ takes in a number and returns an array the length containing the numbers of the Fibonacci Sequence
-
-// ● myNumber › takes in a number and returns an array the length containing the numbers of the Fibonacci Sequence
-
-//   ReferenceError: myNumber is not defined
-// Example input: 6
-// Expected output: [1, 1, 2, 3, 5, 8]
-// describe("input",() =>  {
-//     //     //a test/it method, nested within the describe block that in plain words describes the function that does it 
-//     it("takes the input number 6", () => {
-    
-//     //     //an expect method, nested within the test block, calling the myNumber () function followed by the to.Equal
-//     expect(input10()).toEqual("1, 1, 2, 3, 5, 8, 13, 21, 34, 55")
-//        })
-//      })
-    
-     // FAIL  ./code-challenges.test.js
-// input
-//   ✕ takes the input number 6
-// ● input › takes the input number 6
-//   ReferenceError: input is not defined
+// fibonnaci
+//   ✕ takes in a number greater then 2 and returns an array that length containing the numbers of the Fibonacci Sequence
+// ● fibonnaci › takes in a number greater then 2 and returns an array that length containing the numbers of the Fibonacci Sequence
+//   ReferenceError: myFibonacci is not defined
 
 // b) Create the function that makes the test pass.
 
-const input = () => {
-return "1, 1, 2, 3, 5, 8"
+//Pseudo Code
+// Create a function takes in a number, x
+// the number must be greater than 2, x > 2
+// in that function create an array that can have numbers in it.
+// returns an array that lenght
+// the length is the index, so what ever the number is the index has to match the lenght of the array
+// the length of Fibonacci sequence
+// What is Fibonacci sequence? Googled: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+//Since [1,1] this means the next items in the array are i-1,i-2
+//Access indexes with the variable i and addition. 
+//Output is an array the length of the numbers in Fibonacci sequence 
+// Note: Having trouble running test. 
+
+const fibonacciSequence = (number) => {
+  let myArray = [1, 1]
+  for (let i = 1;  i < number; i++) {
+    myArray.push(myArray[i] + myArray [i - 1])
+  }
+  return myArray
 }
 
-//  PASS  ./code-challenges.test.js
-//   input
-//     ✓ takes the input number 6 (1 ms)
+// //Output:
+// - Expected  - 0
+//     + Received  + 1
+//     @@ -3,6 +3,7 @@
+//         1,
+//         2,
+//         3,
+//         5,
+//         8,
+//     +   13,
+      
+//---------------------------------------------------------------------------------------------------------------------------------------------
 
-// Test Suites: 1 passed, 1 total
-// Tests:       1 passed, 1 total
-// Snapshots:   0 total
-// Time:        0.122 s, estimated 1 s
-// Ran all test suites.
-// ✨  Done in 0.73s.
-// learnacademy@LEARNs-Air week-3-assessment-ecostrong % 
 
-// Example input: 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
-describe("input10",() =>  {
+const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola" ]
+// // // //     // Expected output: [-9, 7, 9, 199]
+const fullArr2= ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+// //     //Expected output: [-823, 7, 23]
 
-// //     //a test/it method, nested within the describe block that in plain words describes the function that does it 
-// it("takes the input number 10", () => {=
-// //     //an expect method, nested within the test block, calling the myNumber () function followed by the to.Equal
-expect(input10()).toEqual("1, 1, 2, 3, 5, 8, 13, 21, 34, 55")
- })
+// //Pseudo Code
+// //create a function that takes in an array
+// //returns a new array of only odd numbers
+// //odd numbers are sorted from least to greatest
+
+
+// // // a) Create a test with expect statements for each of the variables provided.
+
+describe("onlyOdds", () => {
+const newArray1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+const newArray2= ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+it ("takes in an array and returns a new array of only odd numbers sorted from least to greatest", () => {
+  expect(onlyOdds(newArray1)).toEqual([-9, 7, 9, 199])
+  expect(onlyOdds(newArray2)).toEqual([-823, 7, 23])
+  })
 })
 
-const input10 = () => {
-return "1, 1, 2, 3, 5, 8, 13, 21, 34, 55"
+// //  FAIL  ./code-challenges.test.js
+// //   onlyOdds
+// //     ✕ takes in an array and retruns a new array of only odd numbers sorted from least to greatest (1 ms)
+
+// //   ● onlyOdds › takes in an array and retruns a new array of only odd numbers sorted from least to greatest
+
+// //     ReferenceError: onlyOdds is not defined
+
+// // // b) Create the function that makes the test pass.
+
+// //Psuedo Code
+// //Create a function that takes in an array 
+// //It iterates on the condition of typeof ==="number"
+// //.sort((a,b) => a ) 
+// //To get odd numbers use modulo 2 
+// //use .sort to sort the array 
+// //sort from least to greatest 
+
+const onlyOdds = (array) => {
+  return array.filter(value => {
+    if (value % 2 !== 0 && typeof (value) === "number") {
+      return array
+    }
+  }).sort((a, b) => a - b)
 }
 
-//  PASS  ./code-challenges.test.js
-//   input10
-//     ✓ takes the input number 10 (1 ms)
+// PASS  ./code-challenges.test.js
+// onlyOdds
+//   ✓ takes in an array and returns a new array of only odd numbers sorted from least to greatest (1 ms)
 
-// Test Suites: 1 passed, 1 total
-// Tests:       1 passed, 1 total
-// Snapshots:   0 total
-// Time:        0.139 s, estimated 1 s
-// Ran all test suites.
-// ✨  Done in 0.76s.
-// learnacademy@LEARNs-Air week-3-assessment-ecostrong % 
 
-// // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest. 
-//Pseudo Code 
-//create a function that takes in an array 
-// []  var array = []
-//returns a new array of only odd numbers  
-//   new array = only odd numbers 
-//odd numbers are sorted from least to greatest 
-// new array.sort method, 2===
-//ie so if array looked like [10,7,1,2,5] it would return only odd numbers so [7,1,5]
-//then it will sort the odd numbers from least to greatest ie [1,5,7]
-// // a) Create a test with expect statements for each of the variables provided.
-
-describe("takes in an array of numbers and strings",() =>  {
-expect(fullArr1()).toEqual("returns a new array of only odd numbers sorted from least to greatest") 
-}       
-
-//Note:Can not run test because of last line. 
-
-const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-Expected output: [-9, 7, 9, 199]
-
-describe("4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola",() => 
-expect(fullArr1()).toEqual("-9, 7, 9, 199") 
-      
-const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-Expected output: [-823, 7, 23]
-
-describe("hello", 7, 23, -823, false, 78, null, "67", 6, "number",() => 
-expect(fullArr1()).toEqual("823, 7, 23") 
-
-// // b) Create the function that makes the test pass.
+//__________________________________________________________________________________________________________________________________________________
 
 // // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
-//Pseudo Code: 
-// Create a function that takes in an array 
-// It returns an array of the accumulating sum 
-// This means it should add up all the numbers in the array +,+ 
-// The array should be all numbers 
-// Iff array is empty, should return and empty array, so an if else than statement 
 
-// // a) Create a test with expect statements for each of the variables provided.
-const numbersToAdd1 = [2, 4, 45, 9]
- Excpected output: [2, 6, 51, 60]
+const numbersToAdd1 = [2,4,45,9]
+// // // //  Excpected output: [2, 6, 51, 60]
+const numbersToAdd2 = [0, 7, -8, 12]
+// // // // Expected output: [0, 7, -1, 11]
+const numbersToAdd3 = []
+// // // Expected output: []
 
-describe(2, 4, 45, 9,() => {
-expect(fullArr1()).toEqual("2, 6, 51, 60") 
-}
-// const numbersToAdd2 = [0, 7, -8, 12]
-// // Expected output: [0, 7, -1, 11]
+//Pseudo Code:
+// Create a function that takes in an array
+// It returns an array of the accumulating sum
+// This means it should add up all the numbers in the array +,+
+// The array should be all numbers
+// If array is empty, should return and empty array, so an if else than statement
 
-describe("0, 7, -8, 12",() => {
-expect(fullArr1()).toEqual("0, 7, -1, 11") 
-}
+// // // a) Create a test with expect statements for each of the variables provided.
 
-// const numbersToAdd3 = []
-// // Expected output: [] 
+describe ("sumAcc", () => { 
+  const numbersToAdd1 = [2,4,45,9]
+  const numbersToAdd2 = [0, 7, -8, 12]
+  const numbersToAdd3 = []
 
-describe("empty array",() => {
-expect(fullArr1()).toEqual("empty array") }
+  it ("that takes in an array and returns an array of the accumulating sum or an empty array", () => {
+    expect(sumAcc(numbersToAdd1)).toEqual([2,6,51,60])
+    expect(sumAcc(numbersToAdd2)).toEqual([0, 7, -1, 11])
+    expect(sumAcc(numbersToAdd3)).toEqual([])
+  })
+})
 
-//b. Create the function that makes the test pass)  
-//Yarn Jest is not running and giving error because of the last line. Not sure why, red at end of sentence. Could not run test accurately. 
+// FAIL  ./code-challenges.test.js
+//   sumAcc
+//     ✕ that takes in an array and returns an array of the accumulating sum or an empty array
+
+//   ● sumAcc › that takes in an array and returns an array of the accumulating sum or an empty array
+
+//     ReferenceError: sumAcc is not defined
+
+
+// //b. Create the function that makes the test passs)
+//Psuedo Code
+// create a function 
+// takes in an array 
+// iteration is the same length as the input map 
+// Input: [2,4,45,9] [0, 7, -8, 12] 
+// Output: [2,4,45,9] [0, 7, -8, 12] 
+
+const sumAcc = (array) => {
+    let sumArr = []
+    return array.map((value,index) => { 
+      if (index > 0) { 
+        return sumArr =value + sumArr
+      } else {
+        return sumArr = value
+      }
+    }) 
+  }
+
+//   PASS  ./code-challenges.test.js
+//   sumAcc
+//     ✓ that takes in an array and returns an array of the accumulating sum or an empty array (2 ms)
+
+// Test Suites: 1 passed, 1 total
